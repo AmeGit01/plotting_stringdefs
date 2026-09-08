22e89c327c906da48659820d871984b55c21d4a3
AmeGit01 <amedeo.repele@unitn.it>
2026-09-08 15:09:11 +0200

    added files

---
+++ plot_file.jl
@@ -0,0 +1,42 @@
+#!/usr/bin/env julia
+
+using Plots, Plots.Measures, CSV, DataFrames, Infiltrator, Glob
+
+function plot_df(df::DataFrame, filename::AbstractString)
+    # Vaiaini = DateTime("2018-10-28 00:00:00", dateformat"yyyy-mm-dd HH:MM:SS")
+    # Vaiaend = DateTime("2018-11-02 19:00:00", dateformat"yyyy-mm-dd HH:MM:SS")
+    # ticks = collect(Vaiaini:Day(1):Vaiaend)
+    # ticks = push!(ticks, Vaiaend)
+    # ticklabels = Dates.format.(ticks, "dd-mm HH:MM")
+
+    j = findlast(==('.'), filename)
+    name = filename[1:j-1]
+
+    plt = plot(df[!, "t [s]"], df[!, "Discharge [m3/s]"];
+        legend=false, 
+        xlabel="Time [s]", 
+        # xticks=(ticks, ticklabels),
+        # xrotation=30,
+        ylabel="Discharge [m³/s]", 
+        title=name,
+        # bottom_margin=5mm
+    )
+    # display(plt)
+    return plt, name
+end
+
+function main()
+    files = glob("*.csv")
+    println("Found $(length(files)) CSV files:", files)
+
+    for file ∈ files
+        df = CSV.read(file, DataFrame)
+        plt, name = plot_df(df, file)
+        outpath = joinpath("images", "$(name).png")
+        savefig(plt, outpath)
+    end
+
+    return nothing
+end
+
+main()
